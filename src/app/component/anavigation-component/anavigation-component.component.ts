5813d2ee1f87429be9c9f903013a18161a96acbb
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-06 07:59:52 +0100

    initial commit

---
+++ src/app/component/anavigation-component/anavigation-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anavigation-component',
+  templateUrl: './anavigation-component.component.html',
+  styleUrls: ['./anavigation-component.component.css']
+})
+export class ANavigationComponentComponent {
+
+}
