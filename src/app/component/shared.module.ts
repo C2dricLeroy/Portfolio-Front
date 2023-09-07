017d56c6ede0049df0e4b4df132445bfeb1542c6
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-07 11:56:43 +0100

    feat(contact): add a contact form TODO: stylize elements, POST request to Server and client data validation

--- src/app/component/shared.module.ts
+++ src/app/component/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavigationHeaderComponent} from "./navigation-header/navigation-header.component";
 import {AppRoutingModule} from "../app-routing.module";
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [NavigationHeaderComponent],
+  declarations: [NavigationHeaderComponent, FooterComponent],
   imports: [
     CommonModule,
     AppRoutingModule,
