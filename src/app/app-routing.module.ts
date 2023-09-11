bc4ae5a931e4e91c6aaf76efd4e2ffb9a8950582
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-11 14:45:36 +0100

    feat(resume): add HTML for the resume page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {AboutComponent} from "./pages/about/about.component";
 import {ProjectsComponent} from "./pages/projects/projects.component";
-import {ResumeComponent} from "./pages/resume/resume.component";
+import {ResumeComponent} from "./pages/about/resume/resume.component";
 import {ArticlesComponent} from "./pages/articles/articles.component";
 import {ContactComponent} from "./pages/contact/contact.component";
 import {TimelineComponent} from "./pages/about/timeline/timeline.component";
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ArticlesComponent } from './pages/articles/articles.component';
-import { ResumeComponent } from './pages/resume/resume.component';
+import { ResumeComponent } from './pages/about/resume/resume.component';
 import { NavigationHeaderComponent } from './component/navigation-header/navigation-header.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import {CommonModule} from "@angular/common";
