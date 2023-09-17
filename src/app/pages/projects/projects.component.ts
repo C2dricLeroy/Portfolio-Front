35219f8eebf5e5b41f0934d10e2a8b70ae40c031
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-17 21:49:09 +0100

    feat(projects): add template for cards

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
+  projects = [
+    {
+      DateDebut: "06/2023",
+      DateFin: "",
+      Name: 'Groovz',
+      Description: 'Groovz est un projet de réseau social musical pour les utilisateurs de Spotify.',
+      Technologies: ['Next.js', 'Nest.js', 'PostGreSQL', 'Prisma' ],
+      URL: 'http://groovz.fr',
+      Github: 'https://github.com/C2dricLeroy/groovz-front2',
+      Statut: 'En cours',
+    },
+    {
+      DateDebut: "01/2023",
+      DateFin: "06/2023",
+      Name: "Discord Simplon",
+      Description: "Création d'un Discord communautaire pour le groupe Simplon. Automatisation de processus métiers au travers de bots.",
+      Technologies: ['Angular', 'Nest.js', 'PostGreSQL', 'Prisma', 'Discord.js', '' ],
+      Github: ''
+    }
+  ]
 }
