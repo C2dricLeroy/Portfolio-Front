510d0d563ea30742c3b902dbac8afb0d9dd52c01
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-18 16:50:33 +0100

    feat(home): add specific meta

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {
+  constructor(private metaService: Meta, private titleService: Title) {}
+
+  ngOnInit() {
+    this.titleService.setTitle('Projets');
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Développeur, Cédric Leroy, TypeScript'},
+      {name: 'description', content: "Page d'accueil et homepage de mon portfolio orienté TypeScript"},
+      {name: 'robots', content: 'index, follow'}
+    ]);
   }
+
 }
