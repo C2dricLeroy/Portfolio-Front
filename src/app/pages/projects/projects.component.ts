6227fc6abce099887688d844d9209eaac0b3e2d2
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-18 16:47:35 +0100

    feat(projects): implement specific meta for SEO

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit{
+  constructor(private metaService: Meta, private titleService: Title) {}
+
+  ngOnInit() {
+    this.titleService.setTitle('Projets');
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Développeur, Cédric Leroy, TypeScript'},
+      {name: 'description', content: "Cette page regroupe l'ensemble de mes projets et réalisations, orienté TypeScript"},
+      {name: 'robots', content: 'index, follow'}
+    ]);
+  }
 
   projects = [
     {
